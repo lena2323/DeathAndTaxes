cb9b6cb6a1d3adaa4fbd877061a051930c28148f
Lena2323 <lena.jovicic23@gmail.com>
2022-12-14 22:43:05 +0100

    Starting to work

--- src/Components/CreateTable.js
+++ src/Components/CreateTable.js
@@ -3,6 +3,9 @@ import React from 'react'
 
 
 export default function CreateTable(props){
+  
+  let inputYearly = parseInt(props.inputYearlyGrossIncome)
+  let inputChildren = parseInt(props.inputNumberOfCHildren)
     return (
     <div className='overflow-x-auto relative shadow-md sm:rounded-lg'>
         <h1>{props.first}</h1>
@@ -30,20 +33,23 @@ export default function CreateTable(props){
 
 
           < tr className=' border text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 '>
-          <td className='border  py-3 px-6'>{props.inputYearlyGrossIncome} <a className='float-right'>20% tax</a></td>
-          <td className='border py-3 px-6 '>{props.inputNumberOfCHildren} <a className='float-right'>20% tax</a></td>
-          <td className='border py-3 px-6'>{props.inputPropertyOwnership} <a className='float-right'>20% tax</a></td>
-          <td className='border py-3 px-6'>{props.inputMaritalStatus} <a className='float-right'>20% tax</a></td>
+          <td className='border  py-3 px-6'>{props.inputYearlyGrossIncome} <a className='float-right'>{(parseInt(props.inputYearlyGrossIncome) >= 10000) ? "Base TAX is 20%" : "Base TAX is 10%"}</a></td>
+          <td className='border py-3 px-6 '>{props.inputNumberOfCHildren} <a className='float-right'></a></td>
+          <td className='border py-3 px-6'>{props.inputPropertyOwnership} <a className='float-right'></a></td>
+          <td className='border py-3 px-6'>{props.inputMaritalStatus} <a className='float-right'></a></td>
           </tr>
 
           < tr className=' border text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 '>
-          <td className='border  py-3 px-6'>{(parseInt(props.inputYearlyGrossIncome) >= 10000) ? "20% tax is "+(props.inputYearlyGrossIncome * 5/100) : "10% tax is "+(parseInt(props.inputYearlyGrossIncome)*10/100)}</td>
+          <td className='border  py-3 px-6'>{
+          (inputYearly >= 10000) ? "With 20% base and all cuts your total tax is "+(props.inputYearlyGrossIncome * 20/100) : (parseInt(inputYearly) < 10000) ? "With 10% base and all cuts your total tax is "+(props.inputYearlyGrossIncome *10/100) : "" }</td>
 
-          <td className='border py-3 px-6 '>{(parseInt(props.inputNumberOfCHildren) >= 1) && ((parseInt(props.inputYearlyGrossIncome) >= 10000)) ? (parseInt(props.inputYearlyGrossIncome  * 5/100)) : (parseInt(props.inputYearlyGrossIncome)*15/100) }  
+          <td className='border py-3 px-6 '>
+            {(parseInt(props.inputNumberOfCHildren) >= 1) ? 
+            props.inputNumberOfCHildren + " child = -10%"  :  "no tax cut"}  
           
-          <a className='float-right'>20% tax</a></td>
-          <td className='border py-3 px-6'><a className='float-right'>20% tax</a></td>
-          <td className='border py-3 px-6'>{props.inputYearlyGrossIncome} <a className='float-right'>20% tax</a></td>
+          <a className='float-right'></a></td>
+          <td className='border py-3 px-6'><a className='float-right'></a></td>
+          <td className='border py-3 px-6'>{props.inputYearlyGrossIncome} <a className='float-right'></a></td>
           </tr>
           </tbody>    
         </table>
