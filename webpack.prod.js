a24d08d08d52a52286e83106b31c5c5204cca3cb
Lena2323 <lena.jovicic23@gmail.com>
2022-12-15 21:13:57 +0100

    test in prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,25 @@
 const { merge } = require('webpack-merge');
+
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
-        test:/(\.css)$/,
+        test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
-},
+  plugins: [
+    
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+}
 );
