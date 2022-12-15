3a81ad02f0d43bdb0e3432665d202f69ef7c4abd
Lena2323 <lena.jovicic23@gmail.com>
2022-12-15 20:09:32 +0100

    Prod mode env test

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,19 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = merge(common, {
-  mode: "production",
-});
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+},
+
+);
